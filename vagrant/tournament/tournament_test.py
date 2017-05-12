47013b367d314123e042d5275f457473df9f2606
Carla Sukeyosi <carlasukeyosi@gmail.com>
2017-05-11 21:44:31 -0700

    removes prints

--- vagrant/tournament/tournament_test.py
+++ vagrant/tournament/tournament_test.py
@@ -17,7 +17,6 @@ def testCount():
     deleteMatches()
     deletePlayers()
     c = countPlayers()
-    print c
     if c == '0':
         raise TypeError(
             "countPlayers should return numeric zero, not string '0'.")
@@ -129,7 +128,6 @@ def testPairings():
     reportMatch(id5, id6)
     reportMatch(id7, id8)
     pairings = swissPairings()
-    print pairings
     if len(pairings) != 4:
         raise ValueError(
             "For eight players, swissPairings should return 4 pairs. Got {pairs}".format(pairs=len(pairings)))
